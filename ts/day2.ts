39f4a0a032e617cf4016e1ffa5699c6af770b24b
joaopedropsilva <83987930+joaopedropsilva@users.noreply.github.com>
2024-01-18 11:11:34 -0300

    chore(ts): commiting possible solutions to day2 and refactor day1.ts

--- ts/day2.ts
+++ ts/day2.ts
@@ -1,4 +1,28 @@
-import { readInput } from './utils'
+import fs from 'node:fs'
+import rdl from 'node:readline'
+import path from 'node:path'
+
+export async function readInput(file_name: string): Promise<string[][] | []> {
+    const INPUT_PATH = path.join(__dirname, `../${file_name}`);
+    const input = fs.createReadStream(INPUT_PATH, { encoding: 'utf-8' });
+    const output: string[] = [];
+
+    return await new Promise((resolve, reject) => {
+        try {
+            const lineReader = rdl.createInterface({ input });
+
+            lineReader.on('line', (line: string) => {
+                output.push(line);
+            });
+
+            return lineReader.on('close', () => {
+                return resolve(output.map((line: string) => line.split(' ')));
+            });
+        } catch (error) {
+            return reject(error);
+        }
+    });
+}
 
 const pOneTable = {
     A: 1,
@@ -12,6 +36,7 @@ const pTwoTable = {
     Z: 3,
 }
 
+// Error must be here
 function evaluateLineScore(line: string[]): number {
     const fpScore = pOneTable[line[0] as keyof typeof pOneTable]; 
     const spScore = pTwoTable[line[1] as keyof typeof pTwoTable]; 
@@ -26,11 +51,11 @@ function evaluateLineScore(line: string[]): number {
 }
 
 (() => {
-    let total = BigInt(0);
+    let total = 0;
 
     readInput('../inputs/two').then((lines) => {
         lines.forEach(line => {
-            total += BigInt(evaluateLineScore(line));
+            total += evaluateLineScore(line);
         });
 
         console.log(total);
